bfec9c86a2a05c53c4e63b3c01f23dd618149af9
Sergio Delgado <giancarlo080700@gmail.com>
2023-10-28 22:04:27 -0500

    advances

--- src/App.jsx
+++ src/App.jsx
@@ -9,54 +9,65 @@ function App() {
   const [weather, setWeather] = useState()
   const [temp, setTemp] = useState()
   const [isLoading, setIsLoading] = useState(true)
-  const success=position =>{
+  const success = position => {
     console.log(position)
-    const obj={
+    const obj = {
       lat: position.coords.latitude,
       lon: position.coords.longitude
     }
     setCoords(obj)
   }
- 
-  useEffect(()=>{
+
+  useEffect(() => {
     setIsLoading(true)
     navigator.geolocation.getCurrentPosition(success)
-  },[])
+  }, [])
 
-  useEffect(()=>{
-    if(coords){
-    const APIKEY='c4ae548cb563d87244ebc374a02dfa91'
-    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
-    axios.get(url)
-    .then(res => { 
-      const celsius = (res.data.main.temp - 273.15).toFixed(1)
-      const fahrenheit = (celsius * 9/5 + 32).toFixed(1)
-      setTemp({celsius,fahrenheit})
-      setWeather(res.data)})
-    
-    .catch(err=> console.log(err))
-    .finally(()=>setIsLoading(false))
+  useEffect(() => {
+    if (coords) {
+      const APIKEY = 'c4ae548cb563d87244ebc374a02dfa91'
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
+      axios.get(url)
+        .then(res => {
+          const celsius = (res.data.main.temp - 273.15).toFixed(1)
+          const fahrenheit = (celsius * 9 / 5 + 32).toFixed(1)
+          setTemp({ celsius, fahrenheit })
+          setWeather(res.data)
+        })
+
+        .catch(err => console.log(err))
+        .finally(() => setIsLoading(false))
     }
-  },[coords])
+  }, [coords])
 
   console.log(weather);
   return (
-    
+
     <div className='app'>
       {
-        isLoading 
-        ? <h2 className='app__loader'>Loading...</h2>
-        :(
-           <WeatherCard
-            weather={weather} 
-            temp={temp}
+        isLoading
+          ? <div class="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
+          : (
+            <WeatherCard
+              weather={weather}
+              temp={temp}
             />
-        )
-      } 
-  </div>
-  
+          )
+      }
+    </div>
+
 
   )
 
-    }
+}
 export default App
+//<h2 className='app__loader'>Loading...</h2>
